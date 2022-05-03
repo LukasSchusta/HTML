00ba3a36237fc4e0c62b1cb4e7800cbf1500bb93
Lucas Schuster <87291338+LukasSchusta@users.noreply.github.com>
2022-05-02 23:32:55 -0300

    ad

--- cronometro/script.js
+++ cronometro/script.js
@@ -1,13 +1,15 @@
 let cron = 0
 let count = 'start'
 let count2 = 0
+var interval
 function cronometro(){
     let minutos = document.getElementById('minutos-id')
     let segundos = document.getElementById('segundos-id')
     let text = document.getElementById('text-id')
     let min = Number(minutos.value)
     let seg = Number(segundos.value)
-    setInterval(function(){
+    interval = setInterval(function(){
+        
         if(min <= 0 && seg <= 0){
             text.innerHTML = 'Finished'
         }
@@ -22,15 +24,20 @@ function cronometro(){
         seg -= 1
         text.innerHTML = `${min}:${seg}`
         
+        
     }, 1000)
 
 
     if(count == 'start'){
         let img = document.getElementById('image-play-or-stop').src='images/pause.svg'
         count = 'pause'
+        interval = clearInterval(1000)
+        
     }else if(count = 'pause'){
         let img = document.getElementById('image-play-or-stop').src='images/play.svg'
         count = 'start'
+        clearInterval(1000)
+        
     }
 
 }
